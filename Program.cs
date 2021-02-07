7842fdcbe2c9d80510818d19b30b0dccb671f9f7
Alex <athanas1@my.wctc.edu>
2021-02-06 19:00:30 -0600

    Added notes/questions

--- Program.cs
+++ Program.cs
@@ -24,10 +24,12 @@ namespace StringInterpolation_
 
             System.Console.WriteLine("8. {0}", today.ToString("yyyy.MM.dd.hh.mm.ss"));
 
+            //pi part of homework
+            //Should I use double or decimal for this in the future?
             double pi = 3.14159265359;
-
+            //using Currency format with c2
             System.Console.WriteLine("1. {0:c2}", pi);
-
+            //using numbers format with n3
             System.Console.WriteLine("2. {0,10:n3}", pi);
 
         }
